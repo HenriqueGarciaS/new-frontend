e0953b241e0fac8294bf0a0654f4a54bd97457b2
Henrique Silva <henrique9811@hotmail.com>
2020-09-14 17:44:38 -0300

    links entre páginas

--- src/components/HeaderServico/HeaderServico.js
+++ src/components/HeaderServico/HeaderServico.js
@@ -5,13 +5,17 @@ import  './HeaderServico.css'
 
 export default class HeaderServico extends Component{
 
+   goBack(){
+       window.location.href = "http://localhost:3000";
+   }
+
 
     render(){
         return (
             <div className = "HeaderServico">
                 <img src = {logo} alt = ""/>
                 <div className = "area-Usuario">
-                <button id = "voltar">Voltar</button>
+                <button id = "voltar" onClickCapture = {this.goBack}>Voltar</button>
                 <img src = "" id = "fotoUsuario" alt = ""/>
                 </div>
             </div>
